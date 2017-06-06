199a4c88950071f6c77003374f15f38cf0558192
WataruMaeda <wm19850716@gmail.com>
2017-06-06 14:18:57 -0700

    Created project

---
+++ speech-en/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speech-en
+//
+//  Created by Wataru Maeda on 2017/06/06.
+//  Copyright © 2017 Wataru Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
